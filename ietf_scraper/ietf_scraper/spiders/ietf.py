d87b9dbc9272d6a5ca23ff359b8f061701ec08c3
pravali96 <pravali96@gmail.com>
2021-05-02 13:16:13 -0700

    tried to update all

--- ietf_scraper/ietf_scraper/spiders/ietf.py
+++ ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -4,7 +4,9 @@ import scrapy
 class IetfSpider(scrapy.Spider):
     name = 'ietf'
     allowed_domains = ['pythonscraping.com']
-    start_urls = ['http://pythonscraping.com/']
+    start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
 
     def parse(self, response):
-        pass
+        #title = response.css('span.title::text').get()
+        title = response.xpath('//span[@class="title"]/text()').get()
+        return {"title": title}
