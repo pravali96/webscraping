2cc1499e6035a728f1ccb2c96e551d63fbbf59c9
pravali96 <pravali96@gmail.com>
2021-05-02 13:21:59 -0700

    with the other info

--- ietf_scraper/ietf_scraper/spiders/ietf.py
+++ ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import w3lib.html
 
 class IetfSpider(scrapy.Spider):
     name = 'ietf'
@@ -7,6 +7,17 @@ class IetfSpider(scrapy.Spider):
     start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
 
     def parse(self, response):
-        #title = response.css('span.title::text').get()
-        title = response.xpath('//span[@class="title"]/text()').get()
-        return {"title": title}
+        return {
+            'number': response.xpath('//span[@class="rfc-no"]/text()').get(),
+            'title': response.xpath('//meta[@name="DC.Title"]/@content').get(),
+            # 'title': response.xpath('//span[@class="title"]/text()').get(),
+            'date': response.xpath('//span[@class="date"]/text()').get(),
+            # 'date': response.xpath('//meta[@name="DC.Date.Issued"]/@content').get(),
+            'description': response.xpath('//meta[@name="DC.Description.Abstract"]/@content').get(),
+            'author': response.xpath('//meta[@name="DC.Creator"]/@content').get(),
+            # 'author': response.xpath('//span[@class="author-name"]/text()').get(),
+            'company': response.xpath('//span[@class="author-company"]/text()').get(),
+            'address': response.xpath('//span[@class="address"]/text()').get(),
+            'text': w3lib.html.remove_tags(response.xpath('//div[@class="text"]').get()),
+            'headings': response.xpath('//span[@class="subheading"]/text()').getall()
+        }
